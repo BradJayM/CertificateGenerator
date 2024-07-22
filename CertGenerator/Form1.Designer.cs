517ebbf1d8c655ccfb6e23917d183b0209484f2b
BradJayM <102154660+BradJayM@users.noreply.github.com>
2024-07-22 15:19:10 +0100

    Created project files

---
+++ CertGenerator/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace CertGenerator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Btn_Generate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Btn_Generate
+            // 
+            this.Btn_Generate.Location = new System.Drawing.Point(293, 146);
+            this.Btn_Generate.Name = "Btn_Generate";
+            this.Btn_Generate.Size = new System.Drawing.Size(75, 23);
+            this.Btn_Generate.TabIndex = 0;
+            this.Btn_Generate.Text = "Generate";
+            this.Btn_Generate.UseVisualStyleBackColor = true;
+            this.Btn_Generate.Click += new System.EventHandler(this.Btn_Generate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Btn_Generate);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Btn_Generate;
+    }
+}
+
