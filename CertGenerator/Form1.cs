aba33b1865bbe28ba4a889233f4f1b97e5173ce9
BradJayM <bradjaymill@gmail.com>
2024-07-26 15:15:52 +0100

    Added opening and copyting the certificate file

--- CertGenerator/Form1.cs
+++ CertGenerator/Form1.cs
@@ -9,6 +9,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Excel = Microsoft.Office.Interop.Excel;
+using Word = Microsoft.Office.Interop.Word;
 using Microsoft.Data.Analysis;
 
 namespace CertGenerator
@@ -21,6 +22,7 @@ namespace CertGenerator
         }
 
         string inputFile;
+        string MasterCert = "C://Users//bradj//OneDrive//Desktop//MASTERCert.docx";
 
         private void Btn_Generate_Click(object sender, EventArgs e)
         {
@@ -34,26 +36,46 @@ namespace CertGenerator
                 
                 if (inputFile.Contains(".xl"))
                 {
-                    
+                    // Some way of converting .xl files to .csv
                 }
                 if (inputFile.Trim() != "")
                 {
                     //readExcel(inputFile);
-                    makeDataFrame(inputFile);
+                    var GradFrame = makeDataFrame(inputFile);
+                    fillCert(GradFrame);
                     
                 }
             }
         }
 
-        private void makeDataFrame(string InFile)
+        private DataFrame makeDataFrame(string InFile)
         {
             var dataFrame = DataFrame.LoadCsv(InFile, ',');
             var Quals = dataFrame.Rows;
 
-            foreach (DataFrameRow r in Quals){
+            foreach (DataFrameRow r in Quals){  // To Test Dataframe outputs
                 MessageBox.Show(r.ToString());
             }
-        }       
+            return dataFrame;
+        }   
+        
+        private void fillCert(DataFrame Graduates)
+        {
+            var app = new Microsoft.Office.Interop.Word.Application();
+            var sourceDoc = app.Documents.Open(MasterCert);
+
+            sourceDoc.ActiveWindow.Selection.WholeStory();
+            sourceDoc.ActiveWindow.Selection.CopyFormat();
+
+            var newDocument = new Microsoft.Office.Interop.Word.Document();
+            newDocument.ActiveWindow.Selection.PasteFormat();
+            newDocument.SaveAs(@"D:\test1.docx");
+
+            sourceDoc.Close();
+            newDocument.Close();
+
+            app.Quit();
+        }
 
     }
 }
